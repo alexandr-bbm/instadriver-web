be9753c4acce9e96fc600a0e4c4d87520181080e
Alexander Gazizov <alexandr.bbm@gmail.com>
2018-04-17 11:34:10 +0300

    Change db to super-smm-test

--- src/redux/createReduxStore.ts
+++ src/redux/createReduxStore.ts
@@ -1,23 +1,23 @@
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import * as store from 'store2';
-import { InstadriverApi } from '../services/instadriverApi/instadriverApi';
+import {InstadriverApi} from '../services/instadriverApi/instadriverApi';
 import {rootReducer} from './rootReducer';
-import { IThunkExtraArgument } from './types';
-import { BrowserStorage } from '../services/browserStorage/browserStorage';
-import * as firebase from "firebase";
+import {IThunkExtraArgument} from './types';
+import {BrowserStorage} from '../services/browserStorage/browserStorage';
+import * as firebase from 'firebase';
 
 export function createReduxStore() {
   const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
   const storage = new BrowserStorage(store);
   firebase.initializeApp({
-    apiKey: "AIzaSyCwaepneHiLH_XHYqLiIvzFYNt9EXOIuGc",
-    authDomain: "instadriver-2688d.firebaseapp.com",
-    databaseURL: "https://instadriver-2688d.firebaseio.com",
-    projectId: "instadriver-2688d",
-    storageBucket: "",
-    messagingSenderId: "1036497946806"
+    apiKey: 'AIzaSyCtH12hQ5w2MdMPew0nMU-zelEPsxIPHpc',
+    authDomain: 'test-c67b0.firebaseapp.com',
+    databaseURL: 'https://test-c67b0.firebaseio.com',
+    projectId: 'test-c67b0',
+    storageBucket: '',
+    messagingSenderId: '1036497946806',
   });
   const api = new InstadriverApi(firebase);
 
